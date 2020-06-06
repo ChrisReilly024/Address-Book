963d2e012bfa534c5f661e160e1a1ad513e23f9f
ChrisReilly024 <creilly024@gmail.com>
2020-06-06 11:51:22 -0400

    full name, last first methods complete

--- contact.rb
+++ contact.rb
@@ -6,7 +6,15 @@ class Contact
         @middle_name = middle_name
         @last_name = last_name
     end
-
     
+    def full_name
+        @middle_name == '' ? "#{first_name} X #{last_name}" : "#{first_name} #{middle_name} #{last_name}"
+    end
+    
+    def last_first
+        @middle_name == '' ? "#{last_name}, #{first_name}" : "#{last_name}, #{first_name} #{middle_name[0]}."
+    end    
 end
+
+
 
